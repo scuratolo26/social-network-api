d5631e12466e18e9ed24ee2595f34ccb0764006f
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-10-12 12:46:47 -0400

    finished routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -49,6 +49,17 @@ const thoughtController = {
             })
             .catch(err => res.status(400).json(err));
     },
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id.' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status9400).json(err);
+    },
     deleteThought({ params }, res) {
         Thought.findOneAndDelete({ _id: parama.id })
             .then(dbThoughtData => {
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+
+module.exports = router;
